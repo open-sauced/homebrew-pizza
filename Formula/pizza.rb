18901819675a70b3d44a570c922b20edc845e3b3
John McBride <john@opensauced.pizza>
2023-10-16 15:43:49 -0600

    fix: Add "version" identifier to pizza brew tap
    Signed-off-by: John McBride <john@opensauced.pizza>

--- Formula/pizza.rb
+++ Formula/pizza.rb
@@ -1,6 +1,7 @@
 class Pizza < Formula
   desc "The OpenSauced command-line tool"
   homepage "https://github.com/open-sauced/pizza-cli"
+  version "v1.0.1"
   url "https://github.com/open-sauced/pizza-cli/archive/v1.0.1.tar.gz"
   sha256 "bb0e8f4c4c2a63c535e28515b1dfc27df8aceb4f3b43bf613362d4e0c156384b"
   license "MIT"
