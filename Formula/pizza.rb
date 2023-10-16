90d5217dcae04d88e0f586e28ab589c9a97da0ad
John McBride <john@opensauced.pizza>
2023-10-16 15:22:37 -0600

    feat: Add v1.0.1 to homebrew formula
    Signed-off-by: John McBride <john@opensauced.pizza>

--- Formula/pizza.rb
+++ Formula/pizza.rb
@@ -1,8 +1,8 @@
 class Pizza < Formula
   desc "The OpenSauced command-line tool"
   homepage "https://github.com/open-sauced/pizza-cli"
-  url "https://github.com/open-sauced/pizza-cli/archive/v0.0.1-rc1.tar.gz"
-  sha256 "aaf37220cf2f619d484bbbd99bcd27854f288ce380bd60f1367d4b845cfca3dd"
+  url "https://github.com/open-sauced/pizza-cli/archive/v1.0.1.tar.gz"
+  sha256 "bb0e8f4c4c2a63c535e28515b1dfc27df8aceb4f3b43bf613362d4e0c156384b"
   license "MIT"
 
   head "https://github.com/open-sauced/pizza.git", branch: "main"
