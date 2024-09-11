6a50113d69bdb8b21a08dcbde009f9665c5b3896
John McBride <john@opensauced.pizza>
2024-09-11 17:47:50 -0600

    fix: Non Ruby class strings are just added right in build command
    Signed-off-by: John McBride <john@opensauced.pizza>

--- Formula/pizza.rb
+++ Formula/pizza.rb
@@ -5,9 +5,6 @@ class Pizza < Formula
   url "https://github.com/open-sauced/pizza-cli/archive/v1.4.0.tar.gz"
   sha256 "61fa3b3d840cbec4837d8ad438663a086fa6ce04352aaf89d50bd0449c7dac5d"
   license "MIT"
-  posthog_public_key "phc_50r35wnPCQAV66xWzLDHPehBx3Sz0AaN5XG6kEOP9MJ"
-  git_short_sha "ae7a2b45f0c90e2bb4bb7f1727f90fd69d1b1d77"
-
   head "https://github.com/open-sauced/pizza-cli.git", branch: "main"
 
   livecheck do
@@ -21,9 +18,9 @@ class Pizza < Formula
     ldflags = %W[
       -s -w
       -X 'github.com/open-sauced/pizza-cli/pkg/utils.Version=#{version}'
-      -X 'github.com/open-sauced/pizza-cli/pkg/utils.Sha=#{git_short_sha}'
+      -X 'github.com/open-sauced/pizza-cli/pkg/utils.Sha=ae7a2b45f0c90e2bb4bb7f1727f90fd69d1b1d77'
       -X 'github.com/open-sauced/pizza-cli/pkg/utils.Datetime=#{Time.now.utc.strftime("%Y-%m-%d-%H:%M:%S")}'
-      -X 'github.com/open-sauced/pizza-cli/pkg/utils.writeOnlyPublicPosthogKey=#{posthog_public_key}'
+      -X 'github.com/open-sauced/pizza-cli/pkg/utils.writeOnlyPublicPosthogKey=phc_50r35wnPCQAV66xWzLDHPehBx3Sz0AaN5XG6kEOP9MJ'
     ].join(" ")
 
     system "go", "build", "-ldflags", ldflags, "-o", "build/pizza"
