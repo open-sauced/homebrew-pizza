421da36cc9c8862a6c6c5ad27d7d38189fc60cdb
John McBride <john@opensauced.pizza>
2024-09-11 16:56:03 -0600

    feat: Pizza CLI v1.4.0
    Signed-off-by: John McBride <john@opensauced.pizza>

--- Formula/pizza.rb
+++ Formula/pizza.rb
@@ -1,12 +1,12 @@
 class Pizza < Formula
   desc "The OpenSauced command-line tool"
   homepage "https://github.com/open-sauced/pizza-cli"
-  version "v1.2.1"
-  url "https://github.com/open-sauced/pizza-cli/archive/v1.3.0.tar.gz"
-  sha256 "5da48a17c0744fc037dfbe2d1ab1414ef6a3cfd9859237c2f2c8d3454c85dbf4"
+  version "v1.4.0"
+  url "https://github.com/open-sauced/pizza-cli/archive/v1.4.0.tar.gz"
+  sha256 "61fa3b3d840cbec4837d8ad438663a086fa6ce04352aaf89d50bd0449c7dac5d"
   license "MIT"
 
-  head "https://github.com/open-sauced/pizza.git", branch: "main"
+  head "https://github.com/open-sauced/pizza-cli.git", branch: "main"
 
   livecheck do
     url :stable
@@ -17,7 +17,7 @@ class Pizza < Formula
 
   def install
     with_env(
-      "GO_LDFLAGS" => "-s -w -X 'github.com/open-sauced/pizza-cli/pkg/utils.writeOnlyPublicPosthogKey=phc_50r35wnPCQAV66xWzLDHPehBx3Sz0AaN5XG6kEOP9MJ' -X 'github.com/open-sauced/pizza-cli/pkg/utils.Version=v1.3.0' -X 'github.com/open-sauced/pizza-cli/pkg/utils.Sha=b1f1b9555957daf6247bccaf08ce476a61d03206' -X 'github.com/open-sauced/pizza-cli/pkg/utils.Datetime=2024-09-06-09:40:02'",
+      "GO_LDFLAGS" => "-s -w -X 'github.com/open-sauced/pizza-cli/pkg/utils.writeOnlyPublicPosthogKey=phc_50r35wnPCQAV66xWzLDHPehBx3Sz0AaN5XG6kEOP9MJ' -X 'github.com/open-sauced/pizza-cli/pkg/utils.Version=v1.4.0' -X 'github.com/open-sauced/pizza-cli/pkg/utils.Sha=ae7a2b45f0c90e2bb4bb7f1727f90fd69d1b1d77' -X 'github.com/open-sauced/pizza-cli/pkg/utils.Datetime=2024-09-11-16:54:14'",
     ) do
       system "go", "build", "-o=build/pizza"
     end
@@ -26,6 +26,6 @@ class Pizza < Formula
   end
 
   test do
-    assert_match "A command line utility for insights, metrics, and all things OpenSauced", shell_output("#{bin}/pizza")
+    assert_match "A command line utility for insights, metrics, and generating CODEOWNERS documentation for your open source projects", shell_output("#{bin}/pizza")
   end
 end
