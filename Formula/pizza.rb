e780dbdfe5d9f44bdc1ce9dfdd81bfac4eefdd03
John McBride <john@opensauced.pizza>
2024-10-11 08:56:38 -0600

    feat: pizza-cli v2.3.0
    Signed-off-by: John McBride <john@opensauced.pizza>

--- Formula/pizza.rb
+++ Formula/pizza.rb
@@ -1,9 +1,9 @@
 class Pizza < Formula
   desc "The OpenSauced command-line tool"
   homepage "https://github.com/open-sauced/pizza-cli"
-  version "v2.1.0"
-  url "https://github.com/open-sauced/pizza-cli/archive/v2.1.0.tar.gz"
-  sha256 "2b353e9f5be42970e28fc0262dff7fe3adfdbb25aedfeb6758b21e6397ecc813"
+  version "v2.3.0"
+  url "https://github.com/open-sauced/pizza-cli/archive/v2.3.0.tar.gz"
+  sha256 "4636b4b931a9e5f50f1adf995aeaffc8e34fb3ac003ac1da548b1987269f0309"
   license "MIT"
   head "https://github.com/open-sauced/pizza-cli.git", branch: "main"
 
@@ -18,7 +18,7 @@ class Pizza < Formula
     ldflags = %W[
       -s -w
       -X 'github.com/open-sauced/pizza-cli/pkg/utils.Version=#{version}'
-      -X 'github.com/open-sauced/pizza-cli/pkg/utils.Sha=a659d86cd3a88d1856f4f1bd345e2b4d92fa4dd5'
+      -X 'github.com/open-sauced/pizza-cli/pkg/utils.Sha=1d4c8f97af7cc02dfbc01ce5927de89bf7c6d3c4'
       -X 'github.com/open-sauced/pizza-cli/pkg/utils.Datetime=#{Time.now.utc.strftime("%Y-%m-%d-%H:%M:%S")}'
       -X 'github.com/open-sauced/pizza-cli/pkg/utils.writeOnlyPublicPosthogKey=phc_50r35wnPCQAV66xWzLDHPehBx3Sz0AaN5XG6kEOP9MJ'
     ].join(" ")
